ee3c27aabb3b094187453806f91b3bc5e0952eaa
Yann Schwartz <abolibibelot@gmail.com>
2011-02-04 10:14:15 +0100

    Added -watch option. ncoffee can now watch a directory tree for *.coffee files changes. Some ugly workarounds in there though.

---
+++ ncoffee/ChangeWatcher.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading;
+
+namespace ncoffee
+{
+    class ChangeWatcher
+    {
+        private readonly object sync = new object();
+        private readonly IDictionary<string, DateTime> lastWrites = new Dictionary<string, DateTime>();
+        private readonly FileSystemWatcher watcher;
+        private readonly Action<string,CompilerOptions> action;
+        private readonly CompilerOptions options;
+
+        public ChangeWatcher(string path, CompilerOptions options, Action<string,CompilerOptions> action)
+        {
+            //Filesystemwatcher watches..directories. So if the path is a file we watch the file dir
+            var isDirectory = (File.GetAttributes(path) & FileAttributes.Directory) == FileAttributes.Directory;
+            var subDirs = isDirectory;
+            
+            string toWatch = path;
+            string filter = "*.coffee";
+
+            if (!isDirectory)
+            {
+                var info = new FileInfo(path);
+                toWatch = info.DirectoryName;
+                filter = info.Name;
+            }
+
+            watcher = new FileSystemWatcher(toWatch, filter) { IncludeSubdirectories = subDirs };
+            this.action = action;
+            this.options = options;
+            watcher.NotifyFilter = NotifyFilters.LastWrite;
+            watcher.Changed += OnChanged;
+        }
+
+        DateTime GetLastWrite(string path)
+        {
+            DateTime last;
+                if (! lastWrites.TryGetValue(path,out last))
+                    last = new DateTime(1,1,1);
+            return last;
+        }
+
+        void SetLastWrite(string path, DateTime last)
+        {
+                lastWrites[path] = last;
+        }
+
+        public void Start()
+        {
+            watcher.EnableRaisingEvents = true;
+        }
+
+        //FIXME:This isn't really safe, a script could change, start compiling
+        //then change again and complete the second compilation first.
+        //The latest compilation would be overwritten...
+        private void OnChanged(object sender, FileSystemEventArgs e)
+        {
+            var path = e.FullPath;
+            var current = new FileInfo(path).LastWriteTime;
+            
+            lock (sync) //We need to serialize access here
+            {
+                var last = GetLastWrite(path);
+                //hacky way to ignore multiple changes
+                //inherent to using a fsw
+                if (current.Subtract(last).TotalMilliseconds < 100)   
+                    return;
+                SetLastWrite(path, current);
+            }
+            action(e.FullPath, options);
+        }
+    }
+}
--- ncoffee/Program.cs
+++ ncoffee/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Threading;
 using CoffeeScript.Compiler;
 using Mono.Options;
 using System.Text;
@@ -11,7 +12,7 @@ namespace ncoffee
     class Program
     {
 
-        const string Version = "1.0.1";
+        const string Version = "1.0.0";
 
         static void Main(string[] args)
         {
@@ -20,6 +21,7 @@ namespace ncoffee
             var p = new OptionSet()
                 .Add("c|compile", "compile to JavaScript and save as .js files", _ => opt.Compile = true)
                 .Add("o=|output=","set the directory for compiled JavaScript", d => opt.OutputDir = d )
+                .Add("w|watch","watch scripts for changes, and recompile", d => opt.Watch = true )
                 .Add("p|print", "print the compiled JavaScript to stdout", _ => opt.Print = true)
                 .Add("b|bare", "compile without the top-level function wrapper", _ => opt.Bare = true)
                 .Add("v|version", "display coffeescript version", _ => DisplayVersion())
@@ -30,60 +32,102 @@ namespace ncoffee
                 DisplayHelp(p);
             }
 
-            string path = null;
-
+            
             try
             {
-                path = p.Parse(args).First();
+                opt.Path = p.Parse(args).First();
             }
             catch (Exception ex)
             {
                 Console.WriteLine("Error parsing arguments: " + ex.Message);
                 DisplayHelp(p);
             }
+            if (!File.Exists(opt.Path) && !Directory.Exists(opt.Path))
+            {
+                Console.WriteLine("Directory or file does not exist: " + opt.Path);
+                Environment.Exit(-1);
+            }
+
 
             if (opt.Help)
                 DisplayHelp(p);
 
+            if (opt.Watch)
+                StartWatching(opt);
 
             IEnumerable<string> toCompile;
-            if (!Directory.Exists(path) && File.Exists(path))
-                toCompile = new[] { path };
+            if (!Directory.Exists(opt.Path) && File.Exists(opt.Path))
+                toCompile = new[] { opt.Path};
             else
-                toCompile = Glob(path, "*.coffee");
-
+                toCompile = Glob(opt.Path, "*.coffee");
             
             if (opt.Compile)
             {
-                foreach (var sourcePath in toCompile)
-                {
-                    var source = File.ReadAllText(sourcePath,Encoding.UTF8);
-                    string result = "";
-                    try
-                    {
-                        result = CoffeeScriptProcessor.Process(source, opt.Bare);
-                    }
-                    catch (Exception ex)
-                    {
-                        Console.WriteLine("Error while compiling " + sourcePath + " :");
-                        Console.WriteLine(ex.Message);
-                    }
-                    
-                    if (opt.Print)
-                        Console.WriteLine(result);
-                    else
-                    {
-                        var dest = (opt.OutputDir == null)
-                                       ? sourcePath
-                                       : Path.Combine(opt.OutputDir, new FileInfo(sourcePath).Name);
-                        
-                        File.WriteAllText(StripExtension(dest) + ".js", result,Encoding.UTF8);
-                    }
-                }
+                CompileMany(toCompile, opt);
                 return;
             }
-
         }
+
+        private static void StartWatching(CompilerOptions opt)
+        {
+            var watcher = new ChangeWatcher(opt.Path,opt,
+                                (path,options) =>
+                                   {
+                                       Console.WriteLine(DateTime.Now + ":" + path + " has changed.");
+                                       CompileMany(new[] {path}, options);
+                                       Console.WriteLine(DateTime.Now + ":" + path + " recompiled");
+                                   });
+            
+            watcher.Start();
+            
+            Console.WriteLine("Press enter to quit");
+            Console.ReadLine();
+            Environment.Exit(0);
+        }
+
+
+
+        private static void CompileMany(IEnumerable<string> toCompile, CompilerOptions opt)
+        {
+            foreach (var sourcePath in toCompile)
+            {
+                string result = Compile(sourcePath, opt);
+
+                if (opt.Print)
+                    Console.WriteLine(result);
+                else
+                {
+                    var dest = (opt.OutputDir == null)
+                                   ? sourcePath
+                                   : Path.Combine(opt.OutputDir, new FileInfo(sourcePath).Name);
+                        
+                    File.WriteAllText(StripExtension(dest) + ".js", result,Encoding.UTF8);
+                }
+            }
+        }
+
+        private static string Compile(string sourcePath, CompilerOptions opt)
+        {
+            //ugly hack. When triggered by the filesystemwatcher (--watch mode), the file may be transiently locked
+            //by another process, and the call to Read would throw. Trying to monitor this in procmon I found at least
+            //three background processes happily opening the file. We should poll and check locking beforehand, but this
+            //will do for now. Wow, that's a big comment/code ratio.
+            Thread.Sleep(100);
+
+            var source = File.ReadAllText(sourcePath,Encoding.UTF8);
+            string result = "";
+            try
+            {
+                result = CoffeeScriptProcessor.Process(source, opt.Bare);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error while compiling " + sourcePath + " :");
+                Console.WriteLine(ex.Message);
+            }
+            return result;
+        }
+
 
         private static void DisplayVersion()
         {
@@ -126,6 +170,8 @@ namespace ncoffee
         public bool Help { get; set; }
         public bool Bare { get; set; }
         public string OutputDir { get; set; }
+        public bool Watch { get; set; }
+        public string Path { get; set; }
     }
 
 }
